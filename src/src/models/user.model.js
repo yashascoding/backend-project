f84ce60fd435c889b3098423d4e72603f9d95db0
yashas <bhagwatyashas30@gmail.com>
2025-10-07 07:45:40 +0530

    added bcrypt

---
+++ src/src/models/user.model.js
@@ -0,0 +1,84 @@
+import mongoose,{Schema} from 'mongoose';
+import bcrypt from bcrypt;
+const userSchema = new Schema(
+    {
+        avatar:{
+            type:{
+                url:String,
+                localPath:String
+            },
+            default:{
+                url:`https://placehold.co/600x400`,
+                localPath:""
+
+            }
+        },
+        username:{
+            type:String,
+            required:true,
+            unique:true,
+            lowercase: true,
+            trim:true,
+            index:true
+        },
+        email:{
+            type:String,
+            required:true,
+            unique:true,
+            lowercase:true,
+            trim:true
+        },
+        fullName:{
+            type:String,
+            trim:true,
+
+        },
+        password:{
+            type:String,
+            required:[true,"Password is  required"]
+        },
+        isEmailVerified:{
+            type:Boolean,
+            default:false
+        },
+
+        refreshToken:{
+            type:String
+
+        },
+        forgotPasswordToken:{
+                type:String
+        },
+
+        forgotPasswordExpiry:{
+                type:String
+        },
+
+        emailVerificationToken:{
+            type:String
+        },
+
+        emailVerificationExpiry:{
+            type:Date
+        },
+
+
+    },
+    {
+        timestamps:true,
+    },
+);
+
+userSchema.pre("save", async function(next) {
+    if(!this.isModified("password")) return next();
+    
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+});
+
+userSchema.methods.isPasswordCorrect= async function (password) {
+        return await bcrypt.compare(password,this.password)
+}
+
+
+export const User=mongoose.model("User",userSchema)
