c5297060e6465613bb92dd75739cc2ed21293665
yashas <bhagwatyashas30@gmail.com>
2025-10-07 08:01:24 +0530

    added tokens

--- src/src/models/user.model.js
+++ src/src/models/user.model.js
@@ -79,6 +79,28 @@ userSchema.pre("save", async function(next) {
 userSchema.methods.isPasswordCorrect= async function (password) {
         return await bcrypt.compare(password,this.password)
 }
-
-
+userSchema.methods.generateAccessToken=function(){
+    return jwt.sign(
+          {
+        _id:this._id,
+        email:this.email,
+        username:this.username
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {expiresIn:process.env.ACCESS_TOKEN_EXPIRY}
+    )
+  
+}
+userSchema.methods.generateRefreshToken=function(){
+    return jwt.sign(
+          {
+        _id:this._id,
+        email:this.email,
+        username:this.username
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {expiresIn:process.env.REFRESH_TOKEN_EXPIRY}
+    )
+  
+}
 export const User=mongoose.model("User",userSchema)
