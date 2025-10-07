a78d9cc8873aa9ca4709338176b344b6f8695a81
yashas <bhagwatyashas30@gmail.com>
2025-10-07 14:43:34 +0530

    sending email using node js

--- src/src/utils/mail.js
+++ src/src/utils/mail.js
@@ -1,4 +1,43 @@
 import Mailgen from "mailgen";
+import nodemailer from "nodemailer";
+
+ const Sendmail=async(option)=>{
+    const mailGenerator=new Mailgen({
+        theme:"default",
+        product:{
+            name:"Task Manager",
+            link:"https://taskmanagelink.com"
+        }
+    })
+    const emailTextual=mailGenerator.generatePlaintext(options.mailgenContent)
+
+    const emailhtml=mailGenerator.generate(options.mailgenContent)
+
+    nodemailer.createTransport({
+        host:process.env.MAILTRAP_SMTP_HOST,
+        port:process.env.MAILTRAP_SMTP_PORT,
+        auth:{
+            user:process.env.MAILTRAP_SMTP_USER,
+            pass:process.env.MAILTRAP_SMTP_PASS
+        }
+
+    })
+
+    const mail={
+        from:"mail.taskmanager@example.com",
+        to:options.email,
+        subject:options.subject,,
+        text:emailTextual,
+        html:emailhtml
+    }
+    try{
+        await WebTransportError.Sendmail(mail)
+    }
+    catch(error){
+        console.error("Email service failed make sure you enter correct credentials in .env file ")
+        console.error("Error",error);
+    }
+ }
 
 const emailVerificationMailgenContent=(username,VerficationUrl)=>{
     return{
